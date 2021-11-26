91f0e05992ba9822b384981b4ddc3aa227b2a060
jmusiel <73840022+jmusiel@users.noreply.github.com>
2021-11-26 22:15:37 +0000

    added low resource finetuning script

---
+++ low_resource/low_resource_finetuning.py
@@ -0,0 +1,53 @@
+import os
+
+
+def main():
+    # datasets = ["RTE", "MRPC", "STS-B", "CoLA"]
+    # models = ["not_debiased", "standard_debiased", "reinit_debiased"]
+    datasets = ["RTE"]
+    models = ["standard_debiased", "reinit_debiased"]
+    splits = [1000, 100, 10, 5, 1]
+    replicates = range(10)
+
+
+    for seed in range(0, 5):
+        for task in datasets:
+            for model in models:
+                if model == "not_debiased":
+                    reinit = ""
+                    optimizer = "--use_bertadam"
+                elif model == "standard_debiased":
+                    reinit = ""
+                    optimizer = "--use_torch_adamw"
+                elif model == "reinit_debiased":
+                    reinit = "--reinit_pooler --reinit_layers 5 "
+                    optimizer = "--use_torch_adamw"
+
+                for split in splits:
+                    split_str = "split"+str(split)
+                    for replicate in replicates:
+                        rep_str = "r"+str(replicate)
+
+                        # split/replicate/task
+                        data_path = "/home/jovyan/working/class_projects/nlp_11711_project/revisit-bert-finetuning/low_resource/low_resource_glue_data/" + split_str + "/" + rep_str + "/" + str(task)
+                        # split/replicate/model/task/seed
+                        output_path = "low_resource/bert_output/" + split_str + "/" + rep_str + "/" + str(model) + "/" + str(task) + "/SEED" + str(seed)
+
+                        bash_str = ""\
+                            + "python run_glue.py "\
+                            + "--model_type bert --model_name_or_path bert-large-uncased --task_name " + str(task) + " "\
+                            + "--do_train --data_dir " + data_path + " --max_seq_length 128 "\
+                            + "--per_gpu_eval_batch_size 64 --weight_decay 0 --seed " + str(seed) + " --fp16 "\
+                            + "--overwrite_output_dir --do_lower_case --per_gpu_train_batch_size 32 "\
+                            + "--gradient_accumulation_steps 1 --logging_steps 0 --num_loggings 10 "\
+                            + "--save_steps 0 --test_val_split " + str(optimizer) + " --cache_dir /home/jovyan/working/class_projects/nlp_11711_project/bert_finetuning_test/cache "\
+                            + "--num_train_epochs 3.0 --warmup_ratio 0.1 --learning_rate 2e-05 "\
+                            + "--output_dir " + output_path + " "\
+                            + reinit\
+
+                        os.chdir("/home/jovyan/working/class_projects/nlp_11711_project/revisit-bert-finetuning")
+                        os.system(bash_str)
+
+
+if __name__ == "__main__":
+    main()
